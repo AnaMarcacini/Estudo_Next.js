b540b71982d2a7b5e5d4c57e6e61c1bbc6d1d8e9
Ana_D <ana.marcacini@maua.br>
2024-03-26 14:01:56 -0300

    aula 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';// importando a fonte do arquivo de fontes
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}
+      </body>
+      {/* By adding Inter to the <body> element, the font will be applied throughout your application. Here, you're also adding the Tailwind antialiased class which smooths out the font. It's not necessary to use this class, but it adds a nice touch. */}
+
+      {/* Navigate to your browser, open dev tools and select the body element. You should see Inter and Inter_Fallback are now applied under styles. */}
+      {/* <body>{children}</body> */}
+
+
     </html>
   );
 }
