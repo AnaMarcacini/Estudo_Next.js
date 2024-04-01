573a5914b882f1fce92a67067e172a2c0cd3d5cf
Ana_D <ana.marcacini@maua.br>
2024-04-01 13:17:47 -0300

    Card agrupando e streaming

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,7 @@
 //   // if the development server is running and visit http://localhost:3000/dashboard. You should see the "Dashboard Page" text.
 
 
-
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
@@ -21,11 +21,11 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data'; // função sql que importa os ultimmos 5 registros
 
 import { Suspense } from 'react';
-import { RevenueChartSkeleton ,LatestInvoicesSkeleton} from '@/app/ui/skeletons';
+import { RevenueChartSkeleton ,LatestInvoicesSkeleton, CardsSkeleton,} from '@/app/ui/skeletons';
 
 export default async function Page() {
   // const revenue = await fetchRevenue(); //To fetch data for the <RevenueChart/> component, import the fetchRevenue function from data.ts and call it inside your component: 
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -38,14 +38,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
         <Card
           title="Total Customers"
           value={numberOfCustomers}
           type="customers"
-        />
+        /> */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         {/* <RevenueChart revenue={revenue}  /> */}
