9b967502c8b492d6ca8ce72982d2ea6aeaad5efa
Ana_D <ana.marcacini@maua.br>
2024-04-03 09:30:15 -0300

    aula 14 atividade 2

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -127,23 +127,58 @@ export async function createInvoice(prevState: State, formData: FormData) {
 // Nos bastidores, as ações do servidor criam um POSTendpoint de API. É por isso que você não precisa criar endpoints de API manualmente ao usar ações do servidor.
 
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
+export async function updateInvoice(id: string,
+    prevState: State,
+    formData: FormData,
+  ) {
+    const validatedFields = UpdateInvoice.safeParse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
     });
-
-    const amountInCents = amount * 100;
-    try {
-        await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-    } catch (error) {
-        return { message: 'Database Error: Failed to Update Invoice.' };
+   
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Update Invoice.',
+      };
     }
+   
+    const { customerId, amount, status } = validatedFields.data;
+    const amountInCents = amount * 100;
+   
+    try {
+      await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+    } catch (error) {
+      return { message: 'Database Error: Failed to Update Invoice.' };
+    }
+   
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
+
+
+
+    // -----------------------OLD-----------------------
+    // const { customerId, amount, status } = UpdateInvoice.parse({
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // });
+
+    // const amountInCents = amount * 100;
+    // try {
+    //     await sql`
+    //   UPDATE invoices
+    //   SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    //   WHERE id = ${id}
+    // `;
+    // } catch (error) {
+    //     return { message: 'Database Error: Failed to Update Invoice.' };
+    // }
+    // revalidatePath('/dashboard/invoices');
+    // redirect('/dashboard/invoices');
 }
